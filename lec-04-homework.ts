97121b443eb5793419504b227ac5b550571520c4
VenyaBrodetskiy <benjamincheg@gmail.com>
2022-06-19 15:18:33 +0300

    upd 19.06

---
+++ lec-04-homework.ts
@@ -0,0 +1,17 @@
+//
+// Exercise 1. Making range array
+//
+function myRange(min: number, max: number, step: number = 1): number[] {
+    let array: number[] = [];
+    for (let i = min; i <= max; i += step) {
+        array.push(i);
+    }
+    return array;
+}
+const numbers1: number[] = myRange(1, 20, 2); 
+console.log('--> Exercise 1 - Making range function');
+console.log('Array created: ', numbers1);
+console.log('--> Exercise 1    finished \n');
+//
+// Ex. 1 finished
+//
